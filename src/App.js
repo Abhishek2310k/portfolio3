278ec79ade99c1323c0f37946dcbd90e8fa54768
PEPEtheFROG2310 <119100241+Abhishek2310k@users.noreply.github.com>
2024-06-10 12:15:50 +0530

    added about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import AOS from 'aos';
+import { useEffect } from 'react';
+
+import 'aos/dist/aos.css';
 
 function App() {
+
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
   return (
     <div className="App">
       <Header/>
       <Hero/>
+      <main id='main'>
+        <About/>
+      </main>
     </div>
   );
 }
